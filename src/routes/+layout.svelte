f725246c196555740e2b6bb6d6da05eee085b054
AntoineDehan <antoine.dehandartial@gmail.com>
2025-12-12 11:14:31 +0100

    feat(pwa): ajout des fichiers PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,39 @@
 <script>
   import "./layout.css";
   import favicon from "$lib/assets/favicon.svg";
+  import { pwaInfo } from "virtual:pwa-info";
+  import { onMount } from "svelte";
+  import { registerSW } from "virtual:pwa-register";
+
+   onMount(async () => {
+    if (pwaInfo) {
+      const { registerSW } = await import('virtual:pwa-register')
+      registerSW({
+        immediate: true,
+        onRegistered(r) {
+          // uncomment following code if you want check for updates
+          // r && setInterval(() => {
+          //    console.log('Checking for sw update')
+          //    r.update()
+          // }, 20000 /* 20s for testing purposes */)
+          console.log(`SW Registered: ${r}`)
+        },
+        onRegisterError(error) {
+          console.log('SW registration error', error)
+        }
+      })
+    }
+  })
+
+  const webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''
+
 
   let { children } = $props();
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+
+  {@html webManifest}
 </svelte:head>
 <header>
   <nav>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    SvelteKitPWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      injectRegister: "auto",
+      manifest: {
+        name: "AnyPolls",
+        display: "standalone",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [],
+      },
+    }),
+  ],
 });
